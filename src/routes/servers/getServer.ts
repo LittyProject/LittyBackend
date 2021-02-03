2c9a7577901a08948dbba47a59a96f95794eeb5d
xIgreq <igomisz@gmail.com>
2021-02-03 12:09:47 +0100

    server.members

--- src/routes/servers/getServer.ts
+++ src/routes/servers/getServer.ts
@@ -5,10 +5,11 @@ export default async function(req: express.Request, res: express.Response) {
     try {
         if(!req.params.id) return res.notFound();
 
-        const server = await db.getServer(req.params.id);
+        let server = await db.getServer(req.params.id);
         if(!server) {
             return res.notFound();
         } else {
+            server.members = await db.getUsersOnServer(server.id);
             return res.success(server);
         }
     } catch(err) {
